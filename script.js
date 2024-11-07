b2356d876de8650a1cfb6751ff64e1d47c21f9c7
Julio Ribera <julio@magiaycardistry.com>
2024-11-07 13:15:31 +0100

    Course Resources

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Set the date we're counting down to
 const countDownDate = new Date("Dec 31, 2024 23:59:59").getTime();
 
-// Update the count down every 1 second
+// Update the countdown every 1 second
 const countdownfunction = setInterval(function () {
   // Get today's date and time
   const now = new Date().getTime();
@@ -11,7 +11,7 @@ const countdownfunction = setInterval(function () {
   // Find the time remaining
   const distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
+  // Time calculations for days, hours, minutes, and seconds
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hours = Math.floor(
     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -19,7 +19,7 @@ const countdownfunction = setInterval(function () {
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the elements with respective IDs
+  // Display the results in the elements with respective IDs
   document.getElementById("days").innerText = days < 10 ? "0" + days : days;
   document.getElementById("hours").innerText = hours < 10 ? "0" + hours : hours;
   document.getElementById("minutes").innerText =
@@ -35,15 +35,17 @@ const countdownfunction = setInterval(function () {
 }, 1000);
 
 // Hamburger Menu Toggle Script
+
+// Select the menu toggle icon and navigation links
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
+// Add event listener to toggle the navigation menu
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("nav-links-active");
-  console.log("clicked");
 });
 
-// Close menu when a link is clicked (optional)
+// Close the menu when a link is clicked (mobile devices)
 navLinks.addEventListener("click", () => {
   if (window.innerWidth <= 768) {
     navLinks.classList.remove("nav-links-active");
