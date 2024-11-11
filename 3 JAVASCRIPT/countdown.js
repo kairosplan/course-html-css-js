c2b4389ad812bf5c452298f0d880ef56ddf01c8f
Julio Ribera <julio@magiaycardistry.com>
2024-11-11 15:30:07 +0100

    DOM Content Loaded Event Listener

--- 3 JAVASCRIPT/countdown.js
+++ 3 JAVASCRIPT/countdown.js
@@ -1,31 +1,33 @@
-const endDate = new Date("Nov 11, 2024 15:22:40").getTime();
+document.addEventListener("DOMContentLoaded", () => {
+  const endDate = new Date("Nov 11, 2025 15:22:40").getTime();
 
-setInterval(countdown, 1000);
+  setInterval(countdown, 1000);
 
-function countdown() {
-  const paragraph = document.getElementById("countdown");
+  function countdown() {
+    const paragraph = document.getElementById("countdown");
 
-  const now = new Date().getTime();
-  const difference = endDate - now;
+    const now = new Date().getTime();
+    const difference = endDate - now;
 
-  if (difference < 0) {
-    paragraph.textContent = "Offer expired";
-    return;
+    if (difference < 0) {
+      paragraph.textContent = "Offer expired";
+      return;
+    }
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    // console.log({ days, hours, minutes, seconds });
+
+    const message = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds left`;
+
+    paragraph.textContent = message;
   }
-
-  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-  // console.log({ days, hours, minutes, seconds });
-
-  const message = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds left`;
-
-  paragraph.textContent = message;
-}
+});
 
 /*
 
